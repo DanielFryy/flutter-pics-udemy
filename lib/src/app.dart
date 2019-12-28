c5f8ba023c53c7c229ff5b8001d7958d07883e38
DanielFryy <dgz1606@gmail.com>
2019-12-28 18:01:34 -0500

    Moving the app widget to a separate file

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
+
+// Create a class that will be our custom widget. This class must extend the 'StatelessWidget' base class
+class App extends StatelessWidget {
+  // Must define a 'build' method that returns the widgets that *this* widget will show
+  Widget build(context) => MaterialApp(
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              print('Hi there!');
+            },
+          ),
+          appBar: AppBar(
+            title: Text('Let\'s see some images'),
+          ),
+        ),
+      );
+}
