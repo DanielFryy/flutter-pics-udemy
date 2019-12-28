260bb47708e464403a4854c5d153ce2fcf695953
DanielFryy <dgz1606@gmail.com>
2019-12-28 18:16:33 -0500

    Refactorization of the  App widget to make it stateful and display a counter on the screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,23 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget {
+  createState() => AppState();
+}
+
 // Create a class that will be our custom widget. This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
+class AppState extends State<App> {
+  int counter = 0;
   // Must define a 'build' method that returns the widgets that *this* widget will show
   Widget build(context) => MaterialApp(
         home: Scaffold(
+          body: Text('$counter'),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () {
-              print('Hi there!');
+              setState(() {
+                counter++;
+              });
             },
           ),
           appBar: AppBar(
