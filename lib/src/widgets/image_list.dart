827d3c1203cd5dae25ffa8f5490de09e5dd222f2
DanielFryy <dgz1606@gmail.com>
2019-12-28 21:21:46 -0500

    Completion of the app

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -8,6 +8,23 @@ class ImageList extends StatelessWidget {
 
   Widget build(context) => ListView.builder(
         itemCount: images.length,
-        itemBuilder: (context, int index) => Image.network(images[index].url),
+        itemBuilder: (context, int index) => buildImage(images[index]),
+      );
+
+  Widget buildImage(ImageModel image) => Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              child: Image.network(image.url),
+              padding: EdgeInsets.only(bottom: 8.0),
+            ),
+            Text(image.title),
+          ],
+        ),
       );
 }
